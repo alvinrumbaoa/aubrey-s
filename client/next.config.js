5fbd153c6abedaa021998c6281fe44267297aecf
Alvin Rumbaoa <alvinrumbaoa@gmail.com>
2022-11-15 17:07:07 -0800

    s

--- client/next.config.js
+++ client/next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   env:{
     MONGO_URL:'mongodb+srv://aubreydb:Senpou_23@aubreys.utmxqoa.mongodb.net/?retryWrites=true&w=majority'
-  }
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
