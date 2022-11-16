321821a4cf68ca82768d4ef40cfd96ad80f23629
Alvin Rumbaoa <alvinrumbaoa@gmail.com>
2022-11-16 00:30:31 -0800

    upload

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,6 +1,6 @@
 import React,{useEffect} from "react";
 import Head from 'next/head'
-import {Flex, Box, Container, Text, AspectRatio, Button, Image, VStack, Stack, HStack, Link, Heading} from '@chakra-ui/react';
+import {Flex, Box, Container,Grid, Text, AspectRatio, Button, Image, VStack, Stack, HStack, Link, Heading} from '@chakra-ui/react';
 import anime from "animejs"
 
 export default function Home() {
@@ -58,17 +58,17 @@ export default function Home() {
      
               <iframe
                 title='naruto'
-                src='https://www.youtube.com/embed/QhBnZ6NPOY0'
+                src='/videos/video.mp4'
                 allowFullScreen width="900" height="400" 
               />
    
       </Flex>
       <Flex h="80vh" background="#FF5975" justify="center">
-        <Heading>Featured Products</Heading>
-
+        <Heading mt={20} color="white" fontSize="90">Featured Products</Heading>
+        {/* cards */}
       </Flex>
       <Flex>
-        <svg class="wave-top" width="100vw" viewBox="0 50 1000 250"> 
+        <svg className="wave-top" width="100vw" viewBox="0 50 1000 250"> 
   				  <path fillRule="evenodd" clipRule="evenodd" d="M0 108.306L50 114.323C100 120.34 200 132.374 300 168.476C400 204.578 500 264.749 600 246.698C700 228.647 800 132.374 900 108.306C1000 84.2382 1100 132.374 1150 156.442L1200 180.51V-8.5451e-06H1150C1100 -8.5451e-06 1000 -8.5451e-06 900 -8.5451e-06C800 -8.5451e-06 700 -8.5451e-06 600 -8.5451e-06C500 -8.5451e-06 400 -8.5451e-06 300 -8.5451e-06C200 -8.5451e-06 100 -8.5451e-06 50 -8.5451e-06H0V108.306Z" fill="#FF5975"/>
 		   	</svg>
 
